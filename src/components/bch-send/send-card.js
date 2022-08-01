8f0625b9c321d44d95b9c36c94ee3a9143822b7b
Chris Troutner <chris.troutner@gmail.com>
2022-08-01 12:19:52 -0700

    fix(send-card): on-the-fly exchange calculation

--- src/components/bch-send/send-card.js
+++ src/components/bch-send/send-card.js
@@ -6,7 +6,7 @@
 import React from 'react'
 import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPaperPlane, faPaste } from '@fortawesome/free-solid-svg-icons'
+import { faPaperPlane, faPaste, faRandom } from '@fortawesome/free-solid-svg-icons'
 
 let _this
 
@@ -15,7 +15,18 @@ class SendCard extends React.Component {
     super(props)
 
     this.state = {
-      appData: props.appData
+      // Wallet and App data passed from parent components.
+      appData: props.appData,
+
+      bchAddr: '',
+      amountUnits: 'USD',
+      amountStr: '',
+      amountUsd: 0, // on-the-fly calculated USD amount.
+      amountBch: 0, // on-the-fly calculated BCH amount.
+
+      // Used to display the opposite units and quantity.
+      oppositeUnits: 'BCH',
+      oppositeQty: 0
     }
 
     _this = this
@@ -42,7 +53,7 @@ class SendCard extends React.Component {
                 <Col xs={10}>
                   <Form>
                     <Form.Group controlId='formBasicEmail' style={{ textAlign: 'center' }}>
-                      <Form.Control type='text' placeholder='bitcoincash:qqlrzp23w08434twmvr4fxw672whkjy0py26r63g3d' onChange={e => this.setState({ textInput: e.target.value })} />
+                      <Form.Control type='text' placeholder='bitcoincash:qqlrzp23w08434twmvr4fxw672whkjy0py26r63g3d' onChange={e => this.setState({ bchAddr: e.target.value })} />
                     </Form.Group>
                   </Form>
                 </Col>
@@ -63,16 +74,24 @@ class SendCard extends React.Component {
                 <Col xs={12}>
                   <Form>
                     <Form.Group controlId='formBasicEmail' style={{ textAlign: 'center' }}>
-                      <Form.Control type='text' onChange={e => this.setState({ textInput: e.target.value })} />
+                      <Form.Control type='text' onChange={this.handleUpdateAmount} value={this.state.amountStr} />
                     </Form.Group>
                   </Form>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={6}>
+                  Units: {this.state.amountUnits} <FontAwesomeIcon icon={faRandom} size='lg' onClick={this.handleSwitchUnits} />
+                </Col>
+                <Col style={{ textAlign: 'right' }}>
+                  {this.state.oppositeUnits}: {this.state.oppositeQty}
                 </Col>
               </Row>
               <br />
 
               <Row>
                 <Col style={{ textAlign: 'center' }}>
-                  <Button>Send</Button>
+                  <Button onClick={this.handleSendBch}>Send</Button>
                 </Col>
               </Row>
 
@@ -81,6 +100,81 @@ class SendCard extends React.Component {
         </Card>
       </>
     )
+  }
+
+  // This is an on-change event handler that updates the amount calculated in
+  // both BCH and USD as the user types.
+  handleUpdateAmount (event) {
+    const amountStr = event.target.value
+    // console.log('amountStr: ', amountStr)
+
+    try {
+      const amountQty = parseFloat(amountStr)
+
+      // console.log('appData: ', _this.state.appData)
+      const bchUsdPrice = _this.state.appData.bchWalletState.bchUsdPrice
+      const bchjs = _this.state.appData.bchWallet.bchjs
+
+      // Initialize local variables
+      let oppositeQty = 0
+      let amountUsd = 0
+      let amountBch = 0
+
+      const currentUnit = _this.state.amountUnits
+      if (currentUnit.includes('USD')) {
+        // Convert USD to BCH
+        oppositeQty = bchjs.Util.floor8(amountQty / bchUsdPrice)
+        amountUsd = amountQty
+        amountBch = oppositeQty
+      } else {
+        // Convert BCH to USD
+        oppositeQty = bchjs.Util.floor2(amountQty * bchUsdPrice)
+        amountUsd = oppositeQty
+        amountBch = amountQty
+      }
+
+      _this.setState({
+        oppositeQty,
+        amountUsd,
+        amountBch,
+        amountStr
+      })
+    } catch (err) {
+      /* exit quietly */
+      console.log('Error: ', err)
+    }
+  }
+
+  // This is a click event handler that toggles the units between BCH and USD.
+  handleSwitchUnits () {
+    // Toggle the unit
+    let newUnit = ''
+    let oppositeUnits = ''
+    const oldUnit = _this.state.amountUnits
+    if (oldUnit.includes('USD')) {
+      newUnit = 'BCH'
+      oppositeUnits = 'USD'
+    } else {
+      newUnit = 'USD'
+      oppositeUnits = 'BCH'
+    }
+
+    // Clear the Amount text box
+
+    _this.setState({
+      amountUnits: newUnit,
+      oppositeUnits,
+      amountStr: '',
+      oppositeQty: 0,
+      amountUsd: 0,
+      amountBch: 0
+    })
+  }
+
+  // Send BCH based to the address in the form, and the amount specified in the
+  // form.
+  async handleSendBch () {
+    console.log('Sending BCH')
   }
 }
 
