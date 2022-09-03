df64714b1d4e6414dbd4bca3c417c71a08e3a0cf
Chris Troutner <chris.troutner@gmail.com>
2022-09-03 12:25:39 -0700

    feat(footer): Retrieving IPFS CID from BCH blockchain

---
+++ src/config/index.js
@@ -0,0 +1,13 @@
+/*
+  Global configuration settings for this app.
+*/
+
+const config = {
+  // Default IPFS CID for the app. This will be overwritten by dynamic lookup.
+  ipfsCid: 'bafybeibya4cwro6rgqfaazqxckcchy6qo5sz2aqc4dx7ptcvpns5peqcz4',
+
+  // BCH address used to point to the latest version of the IPFS CID.
+  appBchAddr: 'bitcoincash:qztv87ppjh82v527jq2drp4u8rzzn63r5cmhth2zzm'
+}
+
+export default config
