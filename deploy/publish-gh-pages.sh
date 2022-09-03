4f10574f2c098ca1248d149ed5f2403d65f8f56b
Chris Troutner <chris.troutner@gmail.com>
2022-09-03 12:35:04 -0700

    Updating GitHub page

--- deploy/publish-gh-pages.sh
+++ deploy/publish-gh-pages.sh
@@ -2,3 +2,10 @@
 
 # Bash shell script to publish the app to GitHub pages.
 pwd
+git checkout gh-pages
+git merge master
+npm run build
+cp -r build docs
+git add -A
+git commit -m "Updating GitHub page"
+git push
