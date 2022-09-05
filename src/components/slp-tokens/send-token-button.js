65f84bf0307e762fb9bb49e96a9a0e839dbf9a79
Chris Troutner <chris.troutner@gmail.com>
2022-09-05 16:15:24 -0700

    commenting out debug statements

--- src/components/slp-tokens/send-token-button.js
+++ src/components/slp-tokens/send-token-button.js
@@ -77,6 +77,9 @@ class SentTokenButton extends React.Component {
         statusMsg: ''
       })
 
+      // Wait a few seconds to let psf-slp-indexer index the token transfer.
+      // await this.state.appData.bchWallet.bchjs.Util.sleep(2000)
+
       await instance.state.refreshTokens()
     } else {
       // Default behavior
@@ -202,7 +205,7 @@ class SentTokenButton extends React.Component {
 
   // Click handler that fires when the user clicks the Max button.
   handleGetMax () {
-    console.log('get max button clicked.')
+    // console.log('get max button clicked.')
 
     // const token = instance.state.token
     // console.log('token: ', token)
@@ -214,7 +217,7 @@ class SentTokenButton extends React.Component {
 
   // Click handler that fires when the user clicks the 'Send' button.
   async handleSendTokens (instance) {
-    console.log('Send button clicked.')
+    // console.log('Send button clicked.')
 
     try {
       instance.setState({
