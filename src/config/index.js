4fc8c454a54767d089e4f41ed5142186c24ed954
Chris Troutner <chris.troutner@gmail.com>
2023-04-27 19:53:38 -0700

    fix(config): Adding rest URL to config file

--- src/config/index.js
+++ src/config/index.js
@@ -15,7 +15,11 @@ const config = {
   apkUrl: 'https://github.com/Permissionless-Software-Foundation/bch-wallet-web3-android/blob/master/apk/bch-web3-wallet.apk',
   ghRepo: 'https://github.com/Permissionless-Software-Foundation/bch-wallet-web3-android',
   radicleUrl: 'https://app.radicle.network/seeds/maple.radicle.garden/rad:git:hnrkd5cjwwb5tzx37hq9uqm5ubon7ee468xcy/remotes/hyyycncbn9qzqmobnhjq9rry6t4mbjiadzjoyhaknzxjcz3cxkpfpc',
-  filecoinRepo: 'https://bafybeihjei6hnl5dxau2ivnilnruqjql5ge6y5ncirljci5tcwnkx4dsju.ipfs.w3s.link/'
+  filecoinRepo: 'https://bafybeihjei6hnl5dxau2ivnilnruqjql5ge6y5ncirljci5tcwnkx4dsju.ipfs.w3s.link/',
+
+  // Default 'Local Back End' URL. See https://CashStack.info for details.
+  restUrl: 'https://free-bch.fullstack.cash'
+  // restUrl: 'http://localhost:5005'
 }
 
 export default config
