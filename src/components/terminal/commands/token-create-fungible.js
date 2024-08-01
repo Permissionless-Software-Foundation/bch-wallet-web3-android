942da6be2e5a8fa48664502f1b05d372ff5b638f
Chris Troutner <chris.troutner@gmail.com>
2024-07-31 19:26:52 -0700

    Successfully created type 1 token

--- src/components/terminal/commands/token-create-fungible.js
+++ src/components/terminal/commands/token-create-fungible.js
@@ -2,7 +2,7 @@
   This command is used to generate a Type 1 fungible SLP token.
 */
 
-import { Buffer } from 'buffer/'
+// import { Buffer } from 'buffer/'
 
 // Local libraries
 // import config from '../../../../config'
@@ -11,7 +11,10 @@ class TokenCreateFungible {
   async createType1 (inObj = {}) {
     try {
       const { wallet, parsedArgs } = inObj
-      const { ticker, tokenName, decimals, qty, url, hash, baton } = parsedArgs
+      const { ticker, tokenName, decimals, qty, url, hash } = parsedArgs
+
+      // For now no baton
+      const baton = null
 
       // Help
       if (parsedArgs.help) {
@@ -19,8 +22,12 @@ class TokenCreateFungible {
           <span>
             <strong>token_create_fungible:</strong><br />
             <p>
-              This command is used to create a new <a href="https://github.com/simpleledger/slp-specifications/blob/master/slp-token-type-1.md"
-              target="_blank">Type 1 (fungible) SLP token</a>. A 'simple NFT'
+              This command is used to create a new{' '}
+              <a
+                href='https://github.com/simpleledger/slp-specifications/blob/master/slp-token-type-1.md'
+                target='_blank' rel='noreferrer'
+              >Type 1 (fungible) SLP token
+              </a>. A 'simple NFT'
               can be created by setting the <code>qty</code> argument to 1 and
               the <code>decimals</code> property set to 0.
             </p>
@@ -33,7 +40,7 @@ class TokenCreateFungible {
               <li><i>decimals</i> - Divisibility of the tokens. Can be 0 to 10. 0 = non-divisible (NFT). Bitcoin uses 8. USD uses 2. Recommended value: 0-2</li><br />
               <li><i>url</i> - (optional) A website or URL associated with the token. Used by PS002 to set immutable data.</li><br />
               <li><i>hash</i> - (optional) a transaction hash. Used by PS002 to set mutable data.</li><br />
-              <li><i>baton</i> - (optional) an address to send minting baton, which allows minting of additional tokens. If not specified no minting baton is created, making a fixed quantity token.</li><br />
+
             </ul>
             <br /><br />
             <strong>Example usage:</strong><br />
@@ -42,37 +49,131 @@ class TokenCreateFungible {
             </code>
           </span>
         )
+        // <li><i>baton</i> - (optional) an address to send minting baton, which allows minting of additional tokens. If not specified no minting baton is created, making a fixed quantity token.</li><br />
 
         return { outMsg }
       }
 
-      if(!ticker) {
+      // Input Validation
+      if (!ticker) {
         return { outMsg: `ticker argument is required. You entered: ${ticker}` }
       }
-
-      if(!tokenName) {
+      if (!tokenName) {
         return { outMsg: `tokenName argument is required. You entered: ${tokenName}` }
       }
-
-      if(!decimals) {
+      if (!decimals) {
         return { outMsg: `decimals argument is required. You entered: ${decimals}` }
       }
-      if(decimals > 10) {
+      if (decimals > 10) {
         return { outMsg: `decimals argument is out of range. Must be 0 to 10. You entered: ${decimals}` }
       }
-
-      if(!qty) {
+      if (!qty) {
         return { outMsg: `qty argument is required. You entered: ${qty}` }
       }
 
       const bchjs = wallet.bchjs
 
+      // Get a UTXO to pay for the transaction
+      const bchUtxos = wallet.utxos.utxoStore.bchUtxos
+      if (bchUtxos.length === 0) throw new Error('No BCH UTXOs available to pay for transaction.')
 
+      // Pay for the tx with the biggest UTXO in the array.
+      const bchUtxo = bchjs.Utxo.findBiggestUtxo(bchUtxos)
+      // console.log(`bchUtxo: ${JSON.stringify(bchUtxo, null, 2)}`)
+
+      // instance of transaction builder
+      const transactionBuilder = new bchjs.TransactionBuilder()
+
+      const originalAmount = bchUtxo.value
+      const vout = bchUtxo.tx_pos
+      const txid = bchUtxo.tx_hash
+
+      // add input with txid and index of vout
+      transactionBuilder.addInput(txid, vout)
+
+      // Set the transaction fee. Manually set for ease of example.
+      const txFee = 550
+
+      // amount to send back to the sending address.
+      // Subtract two dust transactions for minting baton and tokens.
+      const remainder = originalAmount - 546 * 2 - txFee
+
+      // Determine minting baton
+      let mintBaton = null // Default is burn mint baton
+      if (baton) mintBaton = 2 // 2nd output of tx
+
+      // Determine setting for document URL
+      let documentUrl = ''
+      if (url) documentUrl = url
+
+      // Determine setting for document hash
+      let documentHash = ''
+      if (hash) documentHash = hash
+
+      // Generate SLP config object
+      const configObj = {
+        name: tokenName,
+        ticker,
+        documentUrl,
+        decimals,
+        initialQty: qty,
+        documentHash,
+        mintBatonVout: mintBaton
+      }
+
+      // Generate the OP_RETURN entry for an SLP GENESIS transaction.
+      const script = bchjs.SLP.TokenType1.generateGenesisOpReturn(configObj)
+
+      // OP_RETURN needs to be the first output in the transaction.
+      transactionBuilder.addOutput(script, 0)
+
+      // Send dust transaction representing the tokens.
+      const cashAddress = wallet.walletInfo.cashAddress
+      transactionBuilder.addOutput(
+        bchjs.Address.toLegacyAddress(cashAddress),
+        546
+      )
+
+      // Send dust transaction representing minting baton.
+      if (mintBaton) {
+        transactionBuilder.addOutput(
+          bchjs.Address.toLegacyAddress(cashAddress),
+          546
+        )
+      }
+
+      // add output to send BCH remainder of UTXO.
+      transactionBuilder.addOutput(cashAddress, remainder)
+
+      // Generate a keypair from the change address.
+      // const keyPair = bchjs.HDNode.toKeyPair(change)
+      const keyPair = bchjs.ECPair.fromWIF(wallet.walletInfo.privateKey)
+
+      // Sign the transaction with the HD node.
+      let redeemScript
+      transactionBuilder.sign(
+        0,
+        keyPair,
+        redeemScript,
+        transactionBuilder.hashTypes.SIGHASH_ALL,
+        originalAmount
+      )
+
+      // build tx
+      const tx = transactionBuilder.build()
+      // output rawhex
+      const hex = tx.toHex().toString()
+
+      // Broadcast transation to the network
+      const tokenId = await wallet.broadcast({ hex })
+
+      const explorerLink = `https://token.fullstack.cash/?tokenid=${tokenId}`
 
       const outMsg = (
         <span>
           <p>
-            MDA TXID:
+            New token created with this token ID:<br />
+            <a href={explorerLink} rel='noreferrer'>{tokenId}</a>
           </p>
         </span>
       )
